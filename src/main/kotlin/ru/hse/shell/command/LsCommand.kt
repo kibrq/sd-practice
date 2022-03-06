94281c0df63e31bc31978a6875d1341a5aa18083
German Tarabonda <57793086+kot239@users.noreply.github.com>
2022-03-06 20:26:39 +0300

    Add ls command

--- src/main/kotlin/ru/hse/shell/command/LsCommand.kt
+++ src/main/kotlin/ru/hse/shell/command/LsCommand.kt
@@ -2,13 +2,20 @@ package ru.hse.shell.command
 
 import ru.hse.shell.util.ExitCode
 import ru.hse.shell.util.IO
+import ru.hse.shell.util.StreamUtils
+import java.nio.file.Files
+import java.nio.file.Paths
 
 /*
  * Bash's 'ls' analogue: list content of directory.
  */
 class LsCommand : Command {
     override fun perform(args: List<String>, io: IO): ExitCode {
-        val curPath = Paths.get("").toAbsolutePath().toString()
-        List<String> files = File(curPath).walk()
+        val curPath = Paths.get("").toAbsolutePath()
+        val paths = Files.walk(curPath, 1).map { curPath.relativize(it).toString() }.filter {
+            it.isNotEmpty() &&  !it.startsWith(".")
+        }.toList()
+        StreamUtils.writeToStream(io.outputStream, paths.joinToString(separator = "\n"))
+        return ExitCode.success()
     }
 }
