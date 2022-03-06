8d3f87ac0c650526f53df17dbb51cdd9da9bb8a9
Aleksandra Obriadina <xbreathoflife@gmail.com>
2022-03-06 23:44:14 +0300

    more tests

--- src/test/kotlin/ru/hse/shell/command/CdTest.kt
+++ src/test/kotlin/ru/hse/shell/command/CdTest.kt
@@ -1,13 +1,14 @@
-package ru.hse.shell.command;
+package ru.hse.shell.command
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
 import ru.hse.shell.TestUtils
 import ru.hse.shell.util.Environment
 import java.nio.file.Paths
 
-public class CdTest {
+internal class CdTest {
     private val commandTestData = listOf(
             listOf(".") to Pair(Paths.get(System.getProperty("user.dir")).resolve("."), 0),
             listOf("..") to Pair(Paths.get(System.getProperty("user.dir")).resolve(".."), 0),
@@ -24,5 +25,48 @@ public class CdTest {
             assertEquals(expected.second, result.code)
             assertEquals(expected.first, env.getCurrentDirectory())
         }
+    }
+
+    @Test
+    fun `Cd empty`() {
+        val io = TestUtils.mockIO()
+        val command = CdCommand()
+        val env = Environment()
+        val result = command.perform(listOf(), io, env)
+        assertEquals(0, result.code)
+        assertEquals("", io.outputStream.toString())
+
+        val pwd = PwdCommand()
+        val pwdResult = pwd.perform(listOf(), io, env)
+        assertEquals(0, pwdResult.code)
+        assertEquals(System.getProperty("user.home") + System.lineSeparator(), io.outputStream.toString())
+    }
+
+    @Test
+    fun `Cd to non-existing directory`() {
+        val io = TestUtils.mockIO()
+        val command = CdCommand()
+        val env = Environment()
+        val result = command.perform(listOf("hello_there"), io, env)
+        assertEquals(1, result.code)
+    }
+
+
+    @Test fun `Cd backwards and upwards`() {
+        val curDir = Paths.get(System.getProperty("user.dir"), "src")
+        val io = TestUtils.mockIO()
+        val command = CdCommand()
+        val env = Environment()
+        val result = command.perform(listOf("src"), io, env)
+        assertEquals(0, result.code)
+
+        val pwd = PwdCommand()
+        val pwdResult = pwd.perform(listOf(), io, env)
+        assertEquals(0, pwdResult.code)
+        assertEquals(curDir.toString() + System.lineSeparator(), io.outputStream.toString())
+
+        command.perform(listOf(".."), io, env)
+        assertEquals(0, result.code)
+        assertEquals(System.getProperty("user.dir") + "/src/..", env.getCurrentDirectory().toString())
     }
 }
--- src/test/kotlin/ru/hse/shell/command/LsTest.kt
+++ src/test/kotlin/ru/hse/shell/command/LsTest.kt
@@ -1,11 +1,11 @@
-package ru.hse.shell.command;
+package ru.hse.shell.command
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import ru.hse.shell.TestUtils
 import ru.hse.shell.util.Environment
 
-public class LsTest {
+internal class LsTest {
     private val expected = "README.md\n" +
             "gradle\n" +
             "gradlew\n" +
