4e654406d5d1462029ab7abe00f62684946bac60
Ruslan Salkaev <rsalkaev@gmail.com>
2022-04-12 02:19:59 +0300

    Add field to file saving

--- src/main/kotlin/ru/hse/xcv/mapgen/FromFileFieldGenerationStrategy.kt
+++ src/main/kotlin/ru/hse/xcv/mapgen/FromFileFieldGenerationStrategy.kt
@@ -1,9 +1,11 @@
 package ru.hse.xcv.mapgen
 
 import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import ru.hse.xcv.model.Field
 import ru.hse.xcv.util.fieldFromJsonRepresentation
+import ru.hse.xcv.util.fieldToJsonRepresentation
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
@@ -19,5 +21,11 @@ class FromFileFieldGenerationStrategy(
         val sourceFile = File(fileName)
         val json = sourceFile.readText()
         return fieldFromJsonRepresentation(Json.decodeFromString(json))
+    }
+
+    fun saveField(field: Field) {
+        val sourceFile = File(fileName)
+        val json = Json.encodeToString(fieldToJsonRepresentation(field))
+        sourceFile.writeText(json)
     }
 }
