616853cde5635dcab1165ffa883c42c18db78e79
Ruslan Salkaev <rsalkaev@gmail.com>
2022-04-12 13:39:39 +0300

    Fix when

--- src/main/kotlin/ru/hse/xcv/controllers/ActionController.kt
+++ src/main/kotlin/ru/hse/xcv/controllers/ActionController.kt
@@ -6,7 +6,6 @@ import ru.hse.xcv.events.NoneEvent
 import ru.hse.xcv.util.InputManager
 
 import ru.hse.xcv.model.DynamicObject
-import ru.hse.xcv.model.Mob
 import ru.hse.xcv.model.Dragon
 import ru.hse.xcv.model.Maxim
 import ru.hse.xcv.model.Zombie
@@ -26,11 +25,11 @@ class ActionControllerFactory(
 ) {
     fun create(obj: DynamicObject): ActionController {
         return when (obj) {
-            is Mob -> MobController(obj, MobStrategy(), eventFactory)
             is Dragon -> MobController(obj, MobStrategy(), eventFactory)
             is Zombie -> MobController(obj, MobStrategy(), eventFactory)
             is Maxim  -> MobController(obj, MobStrategy(), eventFactory)
             is Hero   -> PlayerController(obj, inputManager, eventFactory)
+            else -> throw IllegalStateException()
         }
     }
 }
