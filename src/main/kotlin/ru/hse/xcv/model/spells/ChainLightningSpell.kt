4e1d0483a451498a3a55cfa85a325c9b731ab0bf
Ruslan Salkaev <rsalkaev@gmail.com>
2022-04-18 01:32:49 +0300

    Update description

--- src/main/kotlin/ru/hse/xcv/model/spells/ChainLightningSpell.kt
+++ src/main/kotlin/ru/hse/xcv/model/spells/ChainLightningSpell.kt
@@ -2,7 +2,7 @@ package ru.hse.xcv.model.spells
 
 class ChainLightningSpell : Spell {
     override val name = "Chain lightning"
-    override val description = "Chains lightning"
+    override val description = "Stuns nearest enemy"
     override var combination = "HJKL"
     override var coolDown = 5
     val durationMillis: Long = 2000
