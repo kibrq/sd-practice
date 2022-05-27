f1d5180b2119133a77ee7884d5688d5f59653eef
Maxim <maksukh@inbox.ru>
2022-05-27 20:46:59 +0300

    Very important fix

--- runner/src/main/kotlin/ru/hse/runner/Main.kt
+++ runner/src/main/kotlin/ru/hse/runner/Main.kt
@@ -4,9 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-
 @SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class RunnerApplication
+
 fun main(args: Array<String>) {
     runApplication<RunnerApplication>(*args)
 }
