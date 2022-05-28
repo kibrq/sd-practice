b40cb691b09fbff1950710efa711dc95c980fc69
Kirill Brilliantov <ki.br178@gmail.com>
2022-05-28 04:23:49 +0300

    Make docker-compose

--- docker/scripts/monitor-api-build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-while [ ! -f api/build/libs/api-0.0.1-SNAPSHOT.jar ]; do sleep 1; done;
--- docker/scripts/monitor-runner-build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-while [ ! -f runner/build/libs/runner-0.0.1-SNAPSHOT.jar ]; do sleep 1; done;
