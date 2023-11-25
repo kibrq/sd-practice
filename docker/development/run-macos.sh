32aa54291d335035819eeb5114614330dd0b1ddf
Maksim Sukhodolskii <maksukh@inbox.ru>
2023-11-25 12:51:43 +0300

    Voprosy?

--- docker/development/run-macos.sh
+++ docker/development/run-macos.sh
@@ -1,3 +1,4 @@
+# Make sure XQuartz is installed.
 # First run in separate process:
 # socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\"
 docker run -it --name hwproj_development -e DISPLAY=docker.for.mac.host.internal:0 -v "$(pwd)":/app hwproj_development
